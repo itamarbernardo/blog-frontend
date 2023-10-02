a90b814031294df755656ccb716fb2393cb81aa0
Itamar Bernardo <itamarbernardo2013@gmail.com>
2023-10-02 08:43:22 -0300

    Add estilo ao HtmlContent

--- src/components/HtmlContent/stories.tsx
+++ src/components/HtmlContent/stories.tsx
@@ -1,21 +1,18 @@
 import { Meta, StoryFn } from '@storybook/react';
 import { HtmlContent, HtmlContentProps } from '.';
+import mock from './mock';
 
 export default {
   title: 'HtmlContent',
   component: HtmlContent,
   args: {
-    html: `Lorem, ipsum dolor sit amet consectetur adipisicing
-      elit. Nam, quam ratione modi impedit quia aut
-      rem cumque illo repellat possimus eius facilis
-      quibusdam dignissimos maxime deserunt distinctio
-      dolorem beatae itaque!`,
+    html: mock,
   },
 } as Meta;
 
 export const Template: StoryFn<HtmlContentProps> = (args) => {
   return (
-    <div>
+    <div style={{ maxWidth: '800px', margin: '0 auto' }}>
       <HtmlContent {...args} />
     </div>
   );
